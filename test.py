0bf2ec1531edc8eec73fd3f84c16ea0a84a0aee3
dyrul237 <67949563+dyrul237@users.noreply.github.com>
2023-01-31 14:07:31 +0800

    Add files via upload

--- test.py
+++ test.py
@@ -1,33 +1,40 @@
-import sys
-import time
-
-sys.path.append('..')
-import cyberfloodDyrulEdit
-
-cfcontroller = "172.18.0.35"
-username = "admin@spirent.com"
-password = "Spirent@123"
+#! /usr/bin/env python3
 
 
+import os
+import requests
+
+keys = ["title", "name", "date", "feedback"]		# key values for directory
+source = '/home/kali/gia_course6_lab2/feedback/' 	# where you put the directory of feedback
+url = "http://localhost/feedback/"			# host url
+feedback_to_post = []
 
 
-print("Checking for connectivity & credentials...", end=" ")
-cfClient = cyberfloodDyrulEdit.CfClient(username=username, password=password, cfcontroller=cfcontroller)
-cfClient.generateToken()
-if cfClient.isLogged():
-    print("success! [" + cfClient.userName + "]")
+files = os.listdir(source)
+#print (files)						# to check the output of list of files
 
 
-else:
-    print("error! Please check your configuration.")
-    
+def iteration(x):					# will iterate each line in a file
+	with open (source+ x ) as f:			# will open the file
+		line_files = f.read().splitlines()	# will read all the contents of the file then 
+	return line_files				# the splitlines() method splits the lines into a 								# list. The splitting is done at line breaks. then 								# return the value to line_files
+	
 
-#Find the specified Test case and execute:
+for x in files:						# will iterate each file in the directory
+	line_files = iteration(x)			# will pass the file to function(iteration) with x parameter
+	feedback_to_post.append(dict(zip(keys, line_files)))
+	
 
-print("-------------------------------------------------------------------------------")
+for entry in feedback_to_post:
+    response = requests.post(url, data=entry)
+    if response.ok:
+        print("loaded entry")
+    else:
+        print(f"load entry error: {response.status_code}")
 
-cfClient.getTestRun()
+	
 
-#cfClient.converttocsv()
+	
+	
 
 
